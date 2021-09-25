e228e8c4afa71041faa6dfba9fc89ee26e1c07cd
Luisa Pereira <lufe.pereira23@gmail.com>
2021-09-25 11:24:04 -0500

    Creación función checkConfirmContrasena
    Se creo la función para validar el campo de confirmar contraseña

--- js/form.js
+++ js/form.js
@@ -1,41 +1,34 @@
 function checkUsername(valor){
     if(valor !=""){
-
         if ((/^\w+([\.-]?)\w+/).test(valor)){
             if(valor.lenght<30){
                 return true;
             }
-            else{
+            else {
                 return false;
             }
         }
-        else{
+        else {
             return false;
         }
-    
-        }
-    else{
+    }
+    else {
         return false;
     }
-
 }
 
 function checkCorreo(valor){
-
     if (valor!=""){
-        if (/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/.test(valor))
-        {
+        if (/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/.test(valor)){
             return true;
         }
-        else{
-           
+        else {
             return false;
         }
     }
-    else{
+    else {
         return false;
     }
-
 }
 
 function checkContrasena(valor){
@@ -43,8 +36,29 @@ function checkContrasena(valor){
 }
 
 function checkConfirmContrasena(valor1, valor2){
-    
+    if(valor1!="" && valor2!=""){
+        if ((/^(?=.\d)(?=.[a-zñ]).[A-ZÑ]/.test(valor1)) && (/^(?=.\d)(?=.[a-zñ]).[A-ZÑ]/.test(valor2))){
+            if ((valor1.length >=8) && (valor2.length>=8)) {
+                if(valor1==valor2){
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+            else {
+               return false;
+            }
+        }
+        else {
+            return false;
+        }
+    }
+    else {
+        return false;
+    }
 }
 
-module.exports.checkUsername=checkUsername;
+module.exports.checkUsername = checkUsername;
 module.exports.checkCorreo = checkCorreo;
+module.exports.checkConfirmContrasena = checkConfirmContrasena;
