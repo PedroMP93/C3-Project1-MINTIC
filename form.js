65369182fe11b3a3a4c721bf5660ed756f8515b1
OmarColorado <omarcmora@gmail.com>
2021-09-25 09:16:47 -0500

    subo mi parte de verificacion del correo mas el moduloexports que pide el ejercicio

---
+++ form.js
@@ -0,0 +1,49 @@
+function checkUsername(valor){
+    if(valor !=""){
+
+        if ((/^\w+([\.-]?)\w+/).test(valor)){
+            if(valor.lenght<30){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        else{
+            return false;
+        }
+    
+        }
+    else{
+        return false;
+    }
+
+}
+
+function checkCorreo(valor){
+    if (valor!=""){
+        if (/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/.test(valor))
+        {
+            return true;
+        }
+        else{
+           
+            return false;
+        }
+    }
+    else{
+        return false;
+    }
+
+}
+
+function checkContrasena(valor){
+
+}
+
+function checkConfirmContrasena(valor1, valor2){
+    
+}
+
+module.exports.checkUsername=checkUsername;
+module.exports.checkCorreo = checkCorreo;
