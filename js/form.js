3123ba1b78b0ca6025db9bddbe5ed602a63ed6d6
ocruzsa <desidme.oc@gmail.com>
2021-09-25 08:36:26 -0500

    validar usuario

--- js/form.js
+++ js/form.js
@@ -1,4 +1,22 @@
 function checkUsername(valor){
+    if(valor !=""){
+
+        if ((/^\w+([\.-]?)\w+/).test(valor)){
+            if(valor.lenght<30){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        else{
+            return false;
+        }
+    
+        }
+    else{
+        return false;
+    }
 
 }
 
@@ -13,3 +31,5 @@ function checkContrasena(valor){
 function checkConfirmContrasena(valor1, valor2){
     
 }
+
+module.exports.checkUsername=checkUsername
