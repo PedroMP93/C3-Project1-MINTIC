68153df0175ce340a7174b6c3a85fe8ca54ae818
Luisa Pereira <lufe.pereira23@gmail.com>
2021-09-29 12:19:17 -0500

    Funcion agregarRegistro
    Se implemento codigo de funcion agregarRegistro()

---
+++ js/registro.js
@@ -0,0 +1,39 @@
+const arregloRegistros = [];
+
+function agregarRegistro(){
+
+    var x = document.getElementById("form-registro");
+    let username = x.elements[0].value ;
+    let correo = x.elements[1].value;
+    let contrasena = x.elements[2].value;
+
+    //Se crea el registro de un nuevo usuario como objeto
+    var registro = {username: username, correo:correo, contrasena:contrasena};
+
+    //Registro se agrega al arreglo
+    arregloRegistros.push(registro);
+
+    console.log("El registro es: "+registro.username+" y correo: "+registro.correo);
+    //console.log(arregloRegistros);
+    listarRegistro(arregloRegistros);
+}
+
+function obtenerUsername(arreglo){
+
+}
+ 
+function filtrarCorreo(arreglo){
+
+}
+
+function listarRegistro(arreglo){
+    if(arreglo.length != null){
+        for(let i=0; i<arreglo.length; i++){
+            let registro = arreglo[i];
+            console.log(arreglo[i] + "<br>");
+        }
+    }
+}
+
+
+module.export.agregarRegistro = agregarRegistro;
