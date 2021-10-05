2e925bafd0ec98d0d104bd85d710b8f45798ae1e
Luisa Pereira <lufe.pereira23@gmail.com>
2021-10-05 10:24:43 -0500

    Update registro.js
    Se regresaron los cambios de la funcion agregarRegistro

--- js/registro.js
+++ js/registro.js
@@ -1,25 +1,21 @@
-var registros = [];
+let registros= []
 
 function agregarRegistro(){
 
-    // let username = document.getElementById("username").value;
-    // registros.push(username);
+    var x = document.getElementById("form-registro");
+    let username = x.elements[0].value ;
+    let correo = x.elements[1].value;
+    let contrasena = x.elements[2].value;
 
-    let username = document.getElementById("username").value ;
-    // let correo = document.getElementById("email").value;
-    // let contrasena = document.getElementById("contrasena").value;
+    //Se crea el registro de un nuevo usuario como objeto
+    var registro = {username: username, correo:correo, contrasena:contrasena};
 
-    // Se crea el registro de un nuevo usuario como objeto
-    // var registro = {username: username, correo: correo, contrasena: contrasena};
+    //Registro se agrega al arreglo
+    registros.push(registro);
 
-    // Registro se agrega al arreglo
-    registros.push(username);
-    // registros.push(correo);
-    // registros.push(contrasena);
-
-    // console.log("El registro es: "+registro.username+" y correo: "+registro.correo);
-    // console.log(arregloRegistros);
-    // listarRegistro(arregloRegistros);
+    //console.log("El registro es: "+registro.username+" y correo: "+registro.correo);
+    //console.log(arregloRegistros);
+    //listarRegistro(registros);
 }
 
 function obtenerUsername(arreglo){
@@ -32,24 +28,29 @@ function obtenerUsername(arreglo){
     }
 
 }
- 
+    
 function filtrarCorreo(arreglo){
-
-    
-let arreglo2 = [];
-for (const correo of registros) {
-    if (correo.correo.indexOf("hotmail.com") >= 0) {
-        arreglo2.push(correo);
-    }
-}    
-console.log(arreglo2);
-return arreglo2;
-    
-
+    let arreglo2 = [];
+    for (const correo of registros) {
+        if (correo.correo.indexOf("hotmail.com") >= 0) {
+            arreglo2.push(correo);
+        }
+    }    
+    console.log(arreglo2);
+    return arreglo2;        
 }
+        
+
+/*function listarRegistro(arreglo){
+    if(arreglo.length != null){
+        for(let i=0; i<arreglo.length; i++){
+            let registro = arreglo[i];
+            console.log(arreglo[i] + "<br>");
+        }
+    }
+}*/
 
 module.exports.registros = registros;
-module.exports.agregarRegistro = agregarRegistro;
-module.exports.obtenerUsername = obtenerUsername;
 module.exports.filtrarCorreo = filtrarCorreo;
-
+module.exports.obtenerUsername = obtenerUsername;
+module.exports.agregarRegistro = agregarRegistro;
